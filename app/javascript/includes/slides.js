c3dd0236b2f2c9bc148de31c8bf5376f030771c3
lies046 <skytree.skysky@gmail.com>
2020-06-26 13:48:51 +0900

    Add Slides

---
+++ app/javascript/includes/slides.js
@@ -0,0 +1,16 @@
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Account.create([
+  {first_name: "hikaru", last_name: "morita", username: "hikaru", email: "hikaru@example.com", password: "password"},
+  {first_name: "risa", last_name: "watanabe", username: "risa", email: "risa@example.com", password: "password"},
+  {first_name: "erika", last_name: "ikuta", username: "erika", email: "erika@example.com", password: "password"},
+  {first_name: "asuka", last_name: "saito", username: "asuka", email: "asuka@example.com", password: "password"},
+  {first_name: "momoko", last_name: "oozono", username: "momoko", email: "momoko@example.com", password: "password"}
+])
